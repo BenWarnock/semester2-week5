67a36a7f96ac95fc31f70c30351c7b6d4cc059ec
BenWarnock <bvqw2790@leeds.ac.uk>
2026-03-24 00:58:34 +0000

    1

--- worksheet/task2/task2.c
+++ worksheet/task2/task2.c
@@ -2,18 +2,43 @@
  * Sem 2, week 5
  *
  * Portfolio submission: Hexadecimal to Decimal Converter
- * Name:
- * ID: 
+ * Name: Ben Warnock
+ * ID: 201852300
  */
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(void){
 	long decimal=0;
 	char hex[9];
+	int valid = 1;
 	
 	printf("Enter a hexadecimal:");
+	fgets(hex, sizeof(hex), stdin);
+	hex[strcspn(hex, "\n")] = '\0';
+
+	for (int i = 0; i< strlen(hex); i++) {
+		hex[i] = toupper(hex[i]);
+		if (!((hex[i] >= '0' && hex[i] <='9') || (hex[i] >= 'A' && hex[i] <= 'F'))) {
+			valid = 0;
+			printf("Error: Invalid Hexadecimal\n");
+			break;
+		}
+	}
+	
+	for (int i=0; i<strlen(hex); i++) {
+		int value;
+	
+		if (hex[i] >= '0' && hex[i] <= '9') {
+			value = hex[i] - '0';
+		} else if(hex[i] >= 'A' && hex[i] <= 'F') {
+			value = hex[i] - 'A' + 10;
+		}
+	
+	decimal = decimal * 16 + value;
+	}
 
 	// if input contains invalid hex digit
 	// printf("Error: Invalid Hexadecimal\n");
